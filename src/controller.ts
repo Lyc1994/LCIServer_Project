df7895b7f72370b3d87bc8fa3d16da8734d2c726
Meike <403478045@qq.com>
2023-03-23 15:58:35 +0800

    测试代码更新

--- src/controller.ts
+++ src/controller.ts
@@ -2,6 +2,12 @@ import { Application } from 'express'
 import url from 'url'
 
 export function controller(app: Application) {
+    app.get('/pub_test', (req, res) => {
+        let params = url.parse(req.url, true).query
+        console.log('/pub_test body:', params)
+        res.send(params)
+    })
+
     app.get('/test_get', (req, res) => {
         let params = url.parse(req.url, true).query
         console.log('/test_get body:', params)
